e7b98f1d3561c0eb1b44520573f912ccdaf54f92
LucasMalinowski <lucasmalino@outlook.com>
2024-08-26 11:42:17 -0300

    Fix login and sign up pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,11 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.debug = true
+
+  config.web_console.whitelisted_ips = '172.21.0.0/16'
+
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
