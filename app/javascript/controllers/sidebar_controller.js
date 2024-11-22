03dab38ee87e3aa31af4c86b1a4c56df98f2f3ac
lucasbrazau <lucasbrazau@fetch.ly>
2024-11-22 07:38:32 -0300

    Sidebar fixes

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -15,11 +15,36 @@ export default class extends Controller {
       this.sidebarUlHelpTarget.classList.toggle("flex-col");
       this.sidebarUlHelpTarget.classList.toggle("items-center");
     }
+
+    // Attach open button functionality if the button exists
+    this.openButton = document.querySelector("#open-sidebar-mobile");
+    if (this.openButton) {
+      this.openButton.addEventListener("click", this.open.bind(this));
+    }
   }
+
+  disconnect() {
+    // Remove event listener when the controller disconnects
+    if (this.openButton) {
+      this.openButton.removeEventListener("click", this.open.bind(this));
+    }
+  }
+
   toggle(e) {
     e.preventDefault();
     this.switchCurrentState();
   }
+
+  close(e) {
+    e.preventDefault();
+    this.element.classList.add("-translate-x-full");
+  }
+
+  open(e) {
+    e.preventDefault();
+    this.element.classList.remove("-translate-x-full");
+  }
+
   switchCurrentState() {
     const newState = this.element.dataset.expanded === "true" ? "false" : "true";
     this.element.dataset.expanded = newState;
@@ -33,7 +58,6 @@ export default class extends Controller {
       this.sidebarUlHelpTarget.classList.toggle("flex-col");
       this.sidebarUlHelpTarget.classList.toggle("items-center");
     }
-    // }
     document.cookie = `sidebar_expanded=${newState}`;
   }
 }
